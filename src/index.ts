59e6673b9c0a4c6f31f6b33daeefc0a8af2d4734
Damian Sire <damiansire@gmail.com>
2024-08-15 22:45:06 -0300

    test: add basic test example

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Returns the input value without any modification.
+ *
+ * @param {any} x - The value to be returned.
+ * @returns {any} The same value that was passed as input.
+ */
+function identity(x: any) {
+  return x;
+}
+
+export default identity;
